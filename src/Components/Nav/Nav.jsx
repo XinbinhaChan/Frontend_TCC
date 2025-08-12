cb3dbf2c650cf214e3d493987981ca89cb8b6324
Matheus Maximiano <maximiano.theus17@gmail.com>
2025-08-12 12:20:51 -0300

    Terminei o inicio do home mobile

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -8,7 +8,7 @@ export function Nav() {
 
     return (
         <header>
-            <nav className="flex items-center md:gap-50 h-24 w-screen bg-nav-claro justify-between">
+            <nav className="flex items-center md:gap-50 h-16 w-screen bg-nav-claro justify-between">
                 
                 <img src={Logo} alt="logo" className="w-10 md:w-18 ml-3"/>
                 <Tema  className="border-2-red-300" />
--- src/Components/Páginas/Home.jsx
+++ src/Components/Páginas/Home.jsx
@@ -9,27 +9,46 @@ import MemoryBot from "../../assets/MemoryBot.png";
 export function Home() {
   return (
     <main>
+
+
       <div
-        className="h-110 relative"
+        className="h-110 relative flex"
         style={{ backgroundImage: `url(${HomeRet})` }}
       >
-        <img
+
+        <div className="mt-6 ml-1.25"> 
+          <h1 className="text-4xl font-h1-mobile font-bold text-texto-branco mb-4.75">
+
+            Memory<strong className="font-h1-mobile text-4xl font-bold  bg-clip-text text-transparent bg-gradient-to-r from-[rgba(32,162,155,155.100)] via-70% to-[rgba(13,67,61,100)]">Bot</strong>
+          </h1>
+
+          <p className="text-xs 
+          font-texto-normal-mobile font-semibold font
+          text-texto-branco w-49 h-16 text-center">O MemoryBot é um robô assistente que ajuda pessoas com Alzheimer com lembretes, localização de objetos e interação simples por voz.</p>
+
+        </div>
+
+        <div>
+          <img
           src={HomeCirc}
           alt="Decorative Circle"
-          className="right-3 w-55.3125 h-26.7525 mt-45 absolute"
+          
+          className="right-3 w-55.3125 h-26.7525 mt-45 absolute border-amber-400 border-2"
         />
 
         <img
           src={HomeClareador}
           alt=""
-          className="right-0 w-60 h-25 mt-59 absolute rotate-170"
+          className="right-0 w-60 h-25 mt-59 absolute rotate-170 border-2 border-red-400"
+
         />
 
         <img
           src={MemoryBot}
           alt="Mobile Memory Bot"
-          className="right-5 w-50 h-80 mt-25 absolute drop-shadow-2xl"
+          className="right-5 w-50 h-80 mt-25 absolute drop-shadow-2xl border-2 border-green-400"
         />
+        </div>
       </div>
     </main>
   );
